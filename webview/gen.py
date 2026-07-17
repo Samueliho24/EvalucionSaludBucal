630031161db299930f648ceba0333f01213810dd
Samuel Chourio <chourio.samuel.24@gmail.com>
2026-07-17 10:29:14 -0400

    Modificacion de los estilos del formulario
    - Correccion de texto de etiquetas
    - Aplicacion de nuevo estilo visual
    - modificacion del Script de generacion

--- webview/gen.py
+++ webview/gen.py
@@ -1,9 +1,12 @@
 from jinja2 import Environment, FileSystemLoader
-from os.path import dirname, abspath
+from os.path import dirname, abspath, join
 
 dir = dirname(abspath(__file__))
 jinja = Environment(loader=FileSystemLoader(dir))
 
 def directed_loop(a, b, reverse):
 	return range(a, b + 1) if not reverse else reversed(range(a, b + 1))
-print(jinja.get_template("form.jinja.html").render(directed_loop=directed_loop))
+
+html = jinja.get_template("form.jinja.html").render(directed_loop=directed_loop)
+with open(join(dir, "assets", "form.html"), "w", encoding="utf-8") as f:
+	f.write(html)
